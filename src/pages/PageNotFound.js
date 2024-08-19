8152e6e1d86271323d9518767ea7a41829fb22da
Dacs Dacallos <dacsstack@gmail.com>
2024-08-18 20:28:08 -0800

    Add page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -7,10 +7,14 @@ export const PageNotFound = () => {
 
   return (
     <section className="flex flex-col rounded-md shadow-lg w-full max-w-[1280p] leading-1.5 p-4 m-3 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-      <p>404 / Page Not Found</p>
+      <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        404 / Page Not Found
+      </p>
       <img src={Logo} alt="Page Not Found" />
       <Link to="/">
-        <button>Back To Home</button>
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          Back To Home
+        </button>
       </Link>
     </section>
   );
