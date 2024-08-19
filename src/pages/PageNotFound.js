d007881ef569706eff87a346b354a9c283c16e16
Dacs Dacallos <dacsstack@gmail.com>
2024-08-18 20:25:28 -0800

    Add page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/page-not-found.jpg";
 import { useTitle } from "../hooks/useTitle";
-import NotFound from "../assets/images/page-not-found.jpg";
 
 export const PageNotFound = () => {
   useTitle("Page Not Found");
 
   return (
-    <section className="pageNotFound">
+    <section className="flex flex-col rounded-md shadow-lg w-full max-w-[1280p] leading-1.5 p-4 m-3 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
       <p>404 / Page Not Found</p>
-      <img src={NotFound} alt="Page Not Found" />
+      <img src={Logo} alt="Page Not Found" />
       <Link to="/">
         <button>Back To Home</button>
       </Link>
     </section>
-  )
-}
+  );
+};
