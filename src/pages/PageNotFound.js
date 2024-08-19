1ab9747a147960b3416b2607a280b6a3baa90954
Dacs Dacallos <dacsstack@gmail.com>
2024-08-19 07:34:43 -0800

    Centered Button

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -7,21 +7,23 @@ export const PageNotFound = () => {
 
   return (
     <section className="border-1 rounded-md py-5 px-10 ">
-      <p className="block mb-2 text-center text-lg font-medium text-gray-900 dark:text-white ">
-        404 / Page Not Found
-      </p>
-      <img
-        src={Logo}
-        alt="Page Not Found"
-        className="h-auto max-w-lg mx-auto rounded-lg shadow-xl dark:shadow-gray-800"
-      />
-      <Link to="/">
-        <button className="h-auto max-w-lg mx-auto p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="h-auto max-w-lg mx-auto  px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Back To Home
-          </span>
-        </button>
-      </Link>
+      <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+        <h2 className="block mb-2 text-center text-lg font-medium text-gray-900 dark:text-white ">
+          404 / Page Not Found
+        </h2>
+        <img
+          src={Logo}
+          alt="Page Not Found"
+          className="h-auto max-w-lg mx-auto rounded-lg shadow-xl dark:shadow-gray-800"
+        />
+        <Link to="/">
+          <button className="relative inline-flex self-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Back To Home
+            </span>
+          </button>
+        </Link>
+      </div>
     </section>
   );
 };
