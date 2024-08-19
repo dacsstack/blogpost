1727765503f9e70d67556e48c448b78e7909886a
Dacs Dacallos <dacsstack@gmail.com>
2024-08-18 21:34:01 -0800

    Change alignment

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -16,7 +16,7 @@ export const PageNotFound = () => {
         className="h-auto max-w-lg mx-auto rounded-lg shadow-xl dark:shadow-gray-800"
       />
       <Link to="/">
-        <button className="h-auto max-w-lg mx-auto p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button className="relative inline-flex self-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Back To Home
           </span>
