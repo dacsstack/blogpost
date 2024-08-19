3f76b41f56840e30b3f2fd39694d56a2fd7f1901
Dacs Dacallos <dacsstack@gmail.com>
2024-08-18 20:30:13 -0800

    Add page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/images/page-not-found.jpg";
+import Logo from "../assets/images/logo.png";
 import { useTitle } from "../hooks/useTitle";
 
 export const PageNotFound = () => {
